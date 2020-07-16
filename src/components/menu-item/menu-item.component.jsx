ebaaa21a741b41c7bb28d502448795d3e277f71e
enthusiastic-ivic <victorakelo95@gmail.com>
2020-07-15 20:28:56 -0700

    divided homepage into smaller components and just styled the menu-item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({title,size,imageUrl}) => (
+  <div className={`menu-item ${size}`} >
+    <div className='background-image' style={
+      { backgroundImage: `url(${imageUrl})` }
+    }/>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">come get yours</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
